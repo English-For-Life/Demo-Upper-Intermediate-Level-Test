122f15e5bb843712e2932dca880c21cdd86cba85
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-31 08:56:23 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,23 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I think it we've been talking ..... , it's time to start doing something.",
+        options:["long enough", "enough long", "such long", "so too long"],
+        answer: 0
+    },
+    {
+        q:"If it hadn't been for your help, we ..... in trouble now.",
+        options:["will be", "would be", "would have been", "were"],
+        answer: 1
+    },
+    {
+        q:"I wonder ..... mind watching this bag for me for a moment.",
+        options:["wether you would", "if would you", "you would", "would you"],
+        answer: 0
+    },
+    {
+        q:"I'm not a big fan of his films. There is too much violence that's over-the-..... .",
+        options:["moon", "hill", "top", "summit"],
+        answer: 2
+    }    
+]
